6174b649a6cf093284b879d8c4f4e559f928ed4d
Francisco Vargas Ruiz <4354486+fvarrui@users.noreply.github.com>
2021-10-19 10:36:12 +0100

    Update Main.java

--- src/main/java/dad/login/Main.java
+++ src/main/java/dad/login/Main.java
@@ -1,4 +1,4 @@
-package dad.login;
+epackage dad.login;
 
 import dad.login.auth.AuthService;
 import dad.login.auth.FileAuthService;
@@ -12,7 +12,7 @@ public class Main {
 		
 		AuthService ldap = useLdap ? new LdapAuthService() : new FileAuthService();
 		System.out.println("Chuck Norris    : " + ldap.login("cnorris", "patada"));
-		System.out.println("Charles Bronson : " + ldap.login("cbronson", "pistola"));
+		System.out.println("Charles Bronson : " + ldap.login("cbronson", "Metrallet@1"));
 		System.out.println("David Carradine : " + ldap.login("dcarradine", "kungfu"));
 		System.out.println("David Carradine : " + ldap.login("dcarradine", "karate"));
 		System.out.println("Michael Knight  : " + ldap.login("mknight", "kit"));
